db95f88a316f5b38b562993bc81ca4dee0a668ae
ChapaBr <42702137+ChapaBr@users.noreply.github.com>
2019-02-26 15:12:25 -0300

    Origin Master

---
+++ class/conn.php
@@ -0,0 +1,22 @@
+<?php
+
+class db {
+	# Host
+	private $host = 'localhost';
+	# Usuario
+	private $usuario = 'root';
+	# Senha
+	private $senha = '';
+	#Banco de dados
+	private $database = 'agenda';
+
+	public function conecta_mysql(){
+		$con = mysqli_connect ($this -> host, $this -> usuario, $this -> senha, $this -> database);
+		mysqli_set_charset($con, 'utf8');
+
+		if(mysqli_connect_errno()){
+			echo "Erro ao conectar com o Banco de Dados MySQL". mysqli_connect_error();
+		}
+		return $con;
+	}
+}
